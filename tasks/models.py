7c5f3ab8d50318b12ce16166009ed367a5118a51
Abdullah al Rahat <abdullahalrahat4261@gmail.com>
2025-02-26 20:27:45 +0600

    useing decouple

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save,pre_save,m2m_changed,post_delete,pre_delete
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
 
 # class Employee
 class Employee(models.Model):
@@ -48,12 +52,34 @@ class TaskDetail(models.Model):
         ('MEDIUM','Medium'),
         ('HIGH','High'),
     )
-    task=models.OneToOneField(Tasks,on_delete=models.CASCADE,related_name="details")
+    task=models.OneToOneField(Tasks,
+                              on_delete=models.DO_NOTHING,
+                              related_name="details")
     # assigned_to=models.CharField(max_length=100)
     priority=models.CharField(max_length=6, choices=PRIORITY_OPTIONS,default='LOW')
     notes=models.TextField(blank=True,null=True)
     def __str__(self):
         return f"Details for Task{self.task.title}"
     
-    
-    
+   #  Signals 
+
+@receiver(m2m_changed,sender=Tasks.assigned_to.through)
+def notify_employees_on_task_created(sender,instance,action,**kwargs):
+    if action=='post_add':
+       assigned_email=[emp.email for emp in instance.assigned_to.all()]
+       
+       send_mail(
+            "New Tasks Assigned",
+            f"You have been assigned to  the task :{instance.title}",
+            "abdullahalrahat4261@gmail.com",
+            assigned_email,
+            fail_silently=False,
+            
+        )
+@receiver(post_delete,sender=Tasks)
+def delete_associate_details(sender,instance,**kwargs):
+    if instance.details:
+        print(isinstance)
+        instance.details.delete()
+        
+        print("The Task has been deleted successfully")
